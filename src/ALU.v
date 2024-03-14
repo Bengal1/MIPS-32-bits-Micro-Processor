73ead475d4f06d89c34c1a0709b5316eb809aabd
Bengal1 <34989887+Bengal1@users.noreply.github.com>
2024-03-14 17:32:09 +0200

    Update ALU.v

--- src/ALU.v
+++ src/ALU.v
@@ -16,17 +16,17 @@
 // Operations - add, addu, sub, subu, and, or, xor, nor, slt, sltu, sll, srl, sra
 // Control signals - carry, zero, sign
 
-module ALU (operation, operandA, operandB, result_out, carry, zero, sign);
+module ALU (operation, operandA, operandB, result, carry, zero, sign);
 
 input clk;
 input [5:0] operation;
 input [31:0] operandA;
 input [31:0] operandB;
 
-output [31:0] result_out;
+output [31:0] result;
 output carry, zero, sign;
 
-reg [31:0] result;
+reg [31:0] res;
 reg c_o, ze, s;
 
 reg signed [31:0] signed_operandA, signed_operandB;
@@ -41,30 +41,30 @@ always @ (*) begin
     signed_operandB <= operandB;
 
     case(operation)
-    add: {c_o, result} = signed_operandA + signed_operandB;
-    addu: {c_o, result} = operandA + operandB;
-    sub: {c_o, result} = signed_operandA - signed_operandB;
-    subu: {c_o, result} = operandA - operandB;
+    add: {c_o, res} = signed_operandA + signed_operandB;
+    addu: {c_o, res} = operandA + operandB;
+    sub: {c_o, res} = signed_operandA - signed_operandB;
+    subu: {c_o, res} = operandA - operandB;
 
-    and_op: result = operandA & operandB;
-    or_op: result = operandA | operandB;
-    xor_op: result = operandA ^ operandB;
-    nor_op: result = operandA ~| operandB;
+    and_op: res = operandA & operandB;
+    or_op: res = operandA | operandB;
+    xor_op: res = operandA ^ operandB;
+    nor_op: res = operandA ~| operandB;
     
-    slt: result = (signed_operandA < signed_operandB)? 32'b1 : 32'b0;
-    sltu: result = (operandA < operandB)? 32'b1 : 32'b0;
+    slt: res = (signed_operandA < signed_operandB)? 32'b1 : 32'b0;
+    sltu: res = (operandA < operandB)? 32'b1 : 32'b0;
     
-    sll: result = operandA << operandB;
-    srl: result = operandA >> operandB;
-    sra: result = signed_operandA >>> operandB;
+    sll: res = operandA << operandB;
+    srl: res = operandA >> operandB;
+    sra: res = signed_operandA >>> operandB;
     
-    default: {c_o, result} = 33'b0;
+    default: {c_o, res} = 33'b0;
     endcase
 end
 
-assign result_out = result;
+assign result = res;
 assign carry = c_o; 
-assign zero = ~(|result_out); 
-assign sign = result_out[31];
+assign zero = ~(|result); 
+assign sign = result[31];
 
 endmodule
